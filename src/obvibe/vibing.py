0db81322fe699a857f296d9e0b0c2da9bd085698
NukP <nukorn.p27@gmail.com>
2024-11-21 15:24:05 +0100

    Add class Identifiers from autopybis

--- src/obvibe/vibing.py
+++ src/obvibe/vibing.py
@@ -3,3 +3,24 @@ This is the main module for this repository
 """
 
 import pybis
+
+class Identifiers:
+    """
+    Class object help with the identification of space, project and experiment in openBIS.
+    """
+    def __init__(self, space_code: str, project_code: str, experiment_code: str) -> None:
+        self.space_code = space_code
+        self.project_code = project_code
+        self.experiment_code = experiment_code
+
+    @property
+    def space_identifier(self) -> str:
+        return self.space_code
+
+    @property
+    def project_identifier(self) -> str:
+        return f"/{self.space_identifier}/{self.project_code}"
+
+    @property
+    def experiment_identifier(self) -> str:
+        return f"{self.project_identifier}/{self.experiment_code}"
