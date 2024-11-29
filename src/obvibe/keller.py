ff9549df97139571c69ce016c5fa139bc67095a7
NukP <nukorn.p27@gmail.com>
2024-11-29 10:54:04 +0100

    Add get_permid_specific_type and with_download_file decorator

--- src/obvibe/keller.py
+++ src/obvibe/keller.py
@@ -5,6 +5,9 @@ This module contain an axiliary functions
 import pybis
 from typing import Union
 import json
+import shutil
+from functools import wraps
+import os
 
 def make_new_property(
     openbis_object: pybis.Openbis,
@@ -86,6 +89,76 @@ def get_metadata_from_json(dir_json: str, path: str) -> Union[str, float]:
     for key in path.split('||'):
         info = info[key]
     return info
+
+def get_permid_specific_type(experiment_name:str, dataset_type:str, openbis_obj:str, default_space:str ='/TEST_SPACE_PYBIS/TEST_UPLOAD') -> str:
+    """Retrieve the permId of a dataset of a specific type in a specific experiment.
+
+    The function will take care of makeing sure the name of all required stuff is in an upper case as requried by OpenBis
+    
+    Args:
+        experiment_name (str): The name of the experiment. For example: 240906_kigr_gen4_01, 
+        dataset_type (str): The type of the dataset. For example: premise_cucumber_raw_json.
+        openbis_obj (str): The openBIS object.
+        default_space (str): The default space to search in.
+        
+    Returns:
+        str: The permId of the dataset.
+    """
+    ob = openbis_obj
+    # Construct the experiment identifier
+    experiment_identifier = f'{default_space}/{experiment_name.upper()}'
+    
+    # Get the experiment object
+    experiment = ob.get_experiment(experiment_identifier)
+    
+    # Retrieve datasets associated with the experiment
+    datasets = experiment.get_datasets()
+    
+    # Filter datasets by type, comparing in uppercase
+    filtered_datasets = [ds for ds in datasets if ds.type.code.upper() == dataset_type.upper()]
+    
+    # Extract permIds from the filtered datasets
+    perm_ids = [ds.permId for ds in filtered_datasets]
+
+    if len(perm_ids) == 0:
+        raise ValueError(f"No datasets of type '{dataset_type}' found in experiment '{experiment_name}'")
+    if len(perm_ids) > 1:
+        raise ValueError(f"Multiple datasets of type '{dataset_type}' found in experiment '{experiment_name}'")
+    
+    return perm_ids[0]
+
+# This trick will download the file, pass the path to the decorated function, and clean up the file afterward.
+def with_downloaded_file(openbis_obj, destination='temp_files'):
+    """
+    Decorator to handle downloading a file, passing its path and permId to the decorated function,
+    and cleaning up the file afterward.
+
+    Args:
+        openbis_obj: The OpenBIS object.
+        destination: The base folder where files will be downloaded.
+
+    Returns:
+        Decorated function that receives the path to the downloaded file and the permId as arguments.
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(perm_id, *args, **kwargs):
+            # Download the dataset
+            dataset = openbis_obj.get_dataset(perm_id)
+            dataset.download(destination=destination, create_default_folders=True)
+            dir_downloaded = os.path.join(destination, perm_id, 'original')
+            downloaded_filename = os.listdir(dir_downloaded)[0]
+            path_downloaded_file = os.path.join(dir_downloaded, downloaded_filename)
+
+            try:
+                # Call the decorated function with the downloaded file path and permId
+                result = func(path_downloaded_file, perm_id, *args, **kwargs)
+            finally:
+                # Clean up: Delete the downloaded files
+                shutil.rmtree(os.path.join(destination, perm_id))
+            return result
+        return wrapper
+    return decorator
 
     
 
